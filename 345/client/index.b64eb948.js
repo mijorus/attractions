8b41aff319748ef40cc88408a3d203525f169975
Bluzzi <Bluzzi@users.noreply.github.com>
2022-06-11 19:15:40 +0000

    deploy: 8f1b4a9ec5121fe0c983aaa21a4e70f028bc2f88

---
+++ 345/client/index.b64eb948.js
@@ -0,0 +1,1 @@
+import"./client.a9d17a70.js";function t(t){const n=t-1;return n*n*n+1}function n(t){return Math.sin(t*Math.PI/2)}export{t as c,n as s};
---
+++ 345/client/legacy/index.b8f49897.js
@@ -0,0 +1,1 @@
+import{ax as r}from"./client.8231d190.js";var a=r.exports;export{a as _};
